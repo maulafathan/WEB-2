c452b7ac97f33f109a9a6a78a6bfbc61e4c11f40
maulafathan <maulafathanlubis@gmail.com>
2025-02-24 11:17:44 +0700

    praktikum01

---
+++ variable_system.php
@@ -0,0 +1,6 @@
+<?php
+echo "Dokumen ROOT : " . $_SERVER["DOCUMENT_ROOT"];
+
+
+
+?>
---
+++ variable_user.php
@@ -0,0 +1,14 @@
+<?php
+// mendefinisikan variabel
+$nama = 'Maula';
+$umur = 19;
+$berat = 55.55;
+
+
+
+echo ' Nama saya ' . $nama;
+echo '<br/>Umur : ' . $umur . ' Tahun ';
+echo '<br/>Berat : ' . $berat;
+
+
+?>
