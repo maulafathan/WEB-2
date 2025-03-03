dbb9d4677865b5cc538bcfb58f7e5f2d3384eef4
maulafathan <maulafathanlubis@gmail.com>
2025-03-03 12:28:07 +0700

    praktikum02

---
+++ praktikum-2b/nilai_siswa.php
@@ -0,0 +1,50 @@
+<?php
+$proses = $_POST['proses'];
+$nama_siswa = $_POST['nama'];
+$mata_kuliah = $_POST['matkul'];
+$nilai_uts = $_POST['nilai_uts'];
+$nilai_uas = $_POST['nilai_uas'];
+$nilai_tugas = $_POST['nilai_tugas'];
+
+/*
+- Mendefinisikan Variabel
+- Nilai Akhir
+- Grade
+- Predikat
+*/
+
+/*
+- MENENTUKAN LULUS ATAU TIDAK MENGGUNAKAN IF ELSE
+- SISWA DINYATAKAN LULUS JIKA NILAI TOTAL dengan presentase 30% UTS, 35% UAS dan TUGAS 35% melebihi 55
+*/
+
+// MENENTUKAN GRADE NILAI MENGGUNAKAN SYNTAX IF ELSE MULTIKONDISI
+/*
+- Grade E : Jika Nilai Akhir 0-35
+- Grade D : Jika Nilai Akhir 36-55
+- Grade C : Jika Nilai Akhir 56-69
+- Grade B : Jika Nilai Akhir 70-84
+- Grade A : Jika Nilai Akhir 85-100
+- Grade I : Jika Nilai Akhir < 0 atau Nilai Akhir > 100
+*/
+
+// MENENTUKAN PREDIKAT NILAI MENGGUNAKAN SYNTAX SWITCH
+/*
+- Predikat Sangat Kurang : Jika Grade E
+- Predikat Kurang : Jika Grade D
+- Predikat Cukup : Jika Grade C
+- Predikat Memuaskan : Jika Grade B
+- Predikat Sangat Memuaskan : Jika Grade A
+- Predikat Tidak Ada : Jika Grade I
+*/
+
+// MENCETAK HASIL
+if (!empty($proses)) {
+    echo 'Proses : ' . $proses;
+    echo '<br/>Nama : ' . $nama_siswa;
+    echo '<br/>Mata Kuliah : ' . $mata_kuliah;
+    echo '<br/>Nilai UTS : ' . $nilai_uts;
+    echo '<br/>Nilai UAS : ' . $nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : ' . $nilai_tugas;
+    // Mencetak Nilai Akhir, Status, Grade, dan Predikat
+}
