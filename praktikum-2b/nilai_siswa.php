cacd27652255a0391e96df60de1e5b307baa53e9
maulafathan <maulafathanlubis@gmail.com>
2025-03-06 13:24:31 +0700

    praktikum02

--- praktikum-2b/nilai_siswa.php
+++ praktikum-2b/nilai_siswa.php
@@ -6,47 +6,63 @@ $nilai_uts = $_POST['nilai_uts'];
 $nilai_uas = $_POST['nilai_uas'];
 $nilai_tugas = $_POST['nilai_tugas'];
 
-if (!empty($proses)) {
-    echo 'Proses : ' . $proses;
-    echo '<br/>Nama : '.$nama_siswa;
-    echo '<br/>Mata Kuliah : '.$mata_kuliah;
-    echo '<br/>Nilai UTS : '.$nilai_uts;
-    echo '<br/>Nilai UAS : '.$nilai_uas;
-    echo '<br/>Nilai Tugas Praktikum : ',$nilai_tugas;
+// Menghitung Nilai Akhir
+$nilai_akhir = ($nilai_uts * 0.3) + ($nilai_uas * 0.35) + ($nilai_tugas * 0.35);
+
+// Menentukan Lulus atau Tidak
+$status = ($nilai_akhir >= 55) ? 'Lulus' : 'Tidak Lulus';
+
+// Menentukan Grade Nilai
+if ($nilai_akhir < 0 || $nilai_akhir > 100) {
+    $grade = 'I'; // Grade I untuk nilai tidak valid
+} elseif ($nilai_akhir >= 85) {
+    $grade = 'A';
+} elseif ($nilai_akhir >= 70) {
+    $grade = 'B';
+} elseif ($nilai_akhir >= 56) {
+    $grade = 'C';
+} elseif ($nilai_akhir >= 36) {
+    $grade = 'D';
+} else {
+    $grade = 'E';
 }
 
-/*
-- Mendefinisikan Variabel
-- Nilai Akhir
-- Grade
-- Predikat
-*/
-
-/*
-- MENENTUKAN LULUS ATAU TIDAK MENGGUNAKAN IF ELSE
-- SISWA DINYATAKAN LULUS JIKA NILAI TOTAL dengan presentase 30% UTS, 35% UAS dan TUGAS 35% melebihi 55
-*/
-
-// MENENTUKAN GRADE NILAI MENGGUNAKAN SYNTAX IF ELSE MULTIKONDISI
-/*
-- Grade E : Jika Nilai Akhir 0-35
-- Grade D : Jika Nilai Akhir 36-55
-- Grade C : Jika Nilai Akhir 56-69
-- Grade B : Jika Nilai Akhir 70-84
-- Grade A : Jika Nilai Akhir 85-100
-- Grade I : Jika Nilai Akhir < 0 atau Nilai Akhir > 100
-*/
-
-// MENENTUKAN PREDIKAT NILAI MENGGUNAKAN SYNTAX SWITCH
-/*
-- Predikat Sangat Kurang : Jika Grade E
-- Predikat Kurang : Jika Grade D
-- Predikat Cukup : Jika Grade C
-- Predikat Memuaskan : Jika Grade B
-- Predikat Sangat Memuaskan : Jika Grade A
-- Predikat Tidak Ada : Jika Grade I
-*/
+// Menentukan Predikat Nilai
+switch ($grade) {
+    case 'A':
+        $predikat = 'Sangat Memuaskan';
+        break;
+    case 'B':
+        $predikat = 'Memuaskan';
+        break;
+    case 'C':
+        $predikat = 'Cukup';
+        break;
+    case 'D':
+        $predikat = 'Kurang';
+        break;
+    case 'E':
+        $predikat = 'Sangat Kurang';
+        break;
+    case 'I':
+        $predikat = 'Tidak Ada';
+        break;
+    default:
+        $predikat = 'Tidak Diketahui';
+        break;
+}
 
 // MENCETAK HASIL
-
+if (!empty($proses)) {
+    echo 'Proses : ' . $proses;
+    echo '<br/>Nama : ' . $nama_siswa;
+    echo '<br/>Mata Kuliah : ' . $mata_kuliah;
+    echo '<br/>Nilai UTS : ' . $nilai_uts;
+    echo '<br/>Nilai UAS : ' . $nilai_uas;
+    echo '<br/>Nilai Tugas Praktikum : ' . $nilai_tugas;
+    echo '<br/>Nilai Akhir : ' . number_format($nilai_akhir, 2);
+    echo '<br/>Status : ' . $status;
+    echo '<br/>Grade : ' . $grade;
+    echo '<br/>Predikat : ' . $predikat;
+}
 ?>
